1460ea8e43cb44e5ca0e8d7c1c95d82f7f6a8b7b
osunx <splendorfreeman1@gmail.com>
2023-10-19 09:22:40 +0000

    New changes to code

--- main.c
+++ main.c
@@ -72,9 +72,9 @@ int main(int argc, char *argv[]) {
 	}**/
 
         /* Check and execute built-in commands */
-        if (stringtwocmp(command, "exit", 4) == 0) {
+       /* if (stringtwocmp(command, "exit", 4) == 0) {
             execute_exit(command);
-        } else if (stringtwocmp(command, "env", 3) == 0) {
+        }*/ else if (stringtwocmp(command, "env", 3) == 0) {
             execute_env();
         } else if (stringtwocmp(command, "cd", 2) == 0) {
             execute_cd(command);
